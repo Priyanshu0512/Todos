b86dba0895a02ec7e3698315ad81228fb512fa70
Priyanshu0512 <priyanshuk0595@gmail.com>
2025-06-14 13:21:23 +0530

    Intial Setup.

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex flex-col">
+      <p className="p-6">hi</p>
+      <p className="p-5">hi2</p>
+    </div>
+  );
+}
+
+export default App;
